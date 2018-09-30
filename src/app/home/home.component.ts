f663cef47287ab438a887cd7b4ccc6e843ec9ebf
password4gitlab <vidya.sheval24@gmail.com>
2018-09-30 13:57:40 +0530

    Added component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-	String : "vidya";
+	String : "viidya";
+	
   constructor() { }
 
   ngOnInit() {
